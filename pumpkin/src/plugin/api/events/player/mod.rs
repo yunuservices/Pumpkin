4cd0fb7559c4b66b11127e4b21687b251b0e54dd
yunuservices <yunuservices@gmail.com>
2026-02-22 15:10:46 +0300

    refactor(event): remove duplicated module prefix in event file names

--- pumpkin/src/plugin/api/events/player/mod.rs
+++ pumpkin/src/plugin/api/events/player/mod.rs
@@ -2,7 +2,7 @@ pub mod player_change_world;
 pub mod player_chat;
 pub mod player_command_send;
 pub mod player_custom_payload;
-pub mod player_fish;
+pub mod fish;
 pub mod player_gamemode_change;
 pub mod player_interact_entity_event;
 pub mod player_interact_event;
